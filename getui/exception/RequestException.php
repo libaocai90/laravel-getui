b4d50d67fe2d10fef2a62dd753a7593b2b8bf835
libaocai <oracle_lee@qq.com>
2021-06-09 15:14:08 +0800

    init

---
+++ getui/exception/RequestException.php
@@ -0,0 +1,16 @@
+<?php
+class RequestException extends Exception
+{
+	var $requestId;
+
+	public function __construct($requestId, $message, $e)
+	{
+		parent::__construct($message, $e);
+		$this->requestId = $requestId;
+	}
+
+	public function getRequestId()
+	{
+		return $this->requestId;
+	}
+}
---
+++ src/Facade/Getui.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Laravel\Getui\Facade;
+
+use Illuminate\Support\Facades\Facade;
+use Laravel\Getui\GetuiService;
+
+/**
+ * 个推对外服务
+ * Class Getui
+ * @package Laravel\Getui\Facade
+ * @method static pushToSingle(string $type, array $data)
+ * @method static pushToApp(string $type, array $data)
+ * @method static pushToList(string $type, array $data)
+ */
+class Getui extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return GetuiService::class;
+    }
+}
